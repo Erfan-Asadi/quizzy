02ad7cb3bb13e9a1d488018c0ee2f0b7542e507c
Erfan Asadi <asadi.love7981@gmail.com>
2023-09-07 14:24:13 +0330

    styling configForm

--- src/components/ConfigQuiz/ConfigForm.jsx
+++ src/components/ConfigQuiz/ConfigForm.jsx
@@ -3,13 +3,34 @@ import { styled } from "styled-components";
 
 const StyledUl = styled.ul`
   padding-inline-start: 5vw;
+  margin-top: 45px;
 
   li {
     font-size: 15px;
     font-weight: 500;
   }
   li::marker {
-    color: red;
+    color: #FCC822;
+  }
+`;
+const StyledForm = styled.form`
+  margin: auto;
+  width: fit-content;
+  margin-top: 57px;
+
+  input {
+    width: 160px;
+    height: 45px;
+    border: 1px solid #d1d1d1;
+    background-color: #fff;
+    font-family: inherit;
+    font-size: 18px;
+    outline-color: #FCC822;
+    padding: 0 8px ;
+
+    &::placeholder {
+      color: #d1d1d1;
+    }
   }
 `;
 
@@ -27,20 +48,19 @@ const ConfigForm = ({ maxAmount = 50, handleAmountValue }) => {
 
   return (
     <div>
-      <form>
+      <StyledForm>
         <input
           type="text"
           max={max}
           placeholder={`max to ${max}`}
           onChange={(e) => changeHandler(e)}
         />
-      </form>
+      </StyledForm>
       <StyledUl className="dangers-list">
         <li>A Maximum of 50 Questions can be retrieved per cal</li>
         {max < 50 && (
           <li>
-            Only{max} questions are available for your chosen difficulty
-            level.
+            Only <u>{max}</u> questions are available for your chosen difficulty level.
           </li>
         )}
       </StyledUl>
